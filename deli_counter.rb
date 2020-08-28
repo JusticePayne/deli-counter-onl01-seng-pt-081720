67e00308e3910d7697cedfc0dd95b0ca59f8edc5
Justice Payne <spikepayne@yahoo.com>
2020-08-28 02:21:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def line(array)
     message = "The line is currently: "
 
    array.each_with_index do |value, index|
-     message += " #{index.to_i+1}. #{value}"
+     message += "#{index.to_i+1}. #{value}"
   end
   puts "#{message}"
   end
