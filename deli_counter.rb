e4f5240341065a464c0ab837ff713bee1dd21b51
Justice Payne <spikepayne@yahoo.com>
2020-08-28 02:30:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -26,3 +26,12 @@ def line(array)
   puts "#{message}"
   end
 end
+
+def take_a_number(array, name)
+  array.push(name)
+  place = array.index(name)
+  puts "Welcome, #{name}. You are number #{array.index(name)+1} in line."
+  return name, place
+end
+
+
