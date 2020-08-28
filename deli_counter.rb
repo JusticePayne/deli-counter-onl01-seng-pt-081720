e15aec597172fd0580d97ddbc095a3c0924d0067
Justice Payne <spikepayne@yahoo.com>
2020-08-28 02:35:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -38,6 +38,6 @@ def now_serving(array)
   if array.empty? == true
     puts "There is nobody currently in line!"
   else array.empty? == false
-    puts "You are #{array.index} in line!"
+    puts "currently serving #{name}."
   end
 end
