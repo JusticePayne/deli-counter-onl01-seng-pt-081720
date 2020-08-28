046a56a8ddfaa63e515ca543d87b72bac973194e
Justice Payne <spikepayne@yahoo.com>
2020-08-28 00:58:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,21 @@
-katz_deli = []
+# if line == 0
+# puts "The line is currently empty"
+# show everyone in current place in line
 
-def line(array, name)
-  if array.length == 0
-    puts "The line is currentlly empty."
+# def take_a_number (array & name)
+# puts name & position (1-3) in line
+
+# def now_serving 
+# if line == 0
+# puts "There is nobody waiting to be served!"
+# elsif 
+# puts the next person in line
+# line.shift that person
+
+
+
+def line(array)
+  if array == 0
+    puts "The line is currently empty."
 
 end
